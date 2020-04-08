94b3dda54820e8c85658c0d5278e402bf220bcb5
ghostwalker13 <artisinedev@gmail.com>
2020-04-08 23:02:25 +0100

    Patch 0.0.3

--- client/client.js
+++ client/client.js
@@ -47,9 +47,23 @@ getElement("#enterGame").addEventListener("click", EnterGame.whenButtonPressed);
 
 const serverListRooms = new Map();
 class ServerListHandler {
-
+	static clearRoomList() {
+		getElement("#matchmaking-scrollingList").innerHTML = "";
+		serverListRooms.clear();
+	}
 	static refreshRoomList() {
 		if (serverListRooms.size > 0) getElement("#matchmaking-empty").style.display = "none";
+	}
+	static updateRoomList() {
+		socket.emit("client->server:request-room-list", {
+			thanks: true
+		}, function(data){
+			ServerListHandler.clearRoomList();
+			const newRooms = new Map(data.rooms);
+			for (const _room of newRooms.values()) {
+				ServerListHandler.deployRoomListObject(_room.id, _room.name, _room.passwordProtected);
+			}
+		});
 	}
 	static deployRoomListObject(roomId, roomName, locked=false) {
 
@@ -70,7 +84,7 @@ class ServerListHandler {
 		divColRoomName.classList.add("col");
 		const pColRoomName = document.createElement("p");
 		pColRoomName.classList.add("d-inline");
-		pColRoomName.textContent = "#" + roomName;
+		pColRoomName.textContent = roomName;
 
 		const divColPasswordLocked = document.createElement("div");
 		divColPasswordLocked.classList.add("col");
@@ -78,6 +92,7 @@ class ServerListHandler {
 		pColPasswordLocked.classList.add("d-inline");
 		pColPasswordLocked.textContent = (locked) ? "🔒" : "Open";
 		pColPasswordLocked.mafia_locked = locked;
+		divRoom.mafia_locked = locked;
 
 		divColRoomId.appendChild(pColRoomId);
 		divColRoomName.appendChild(pColRoomName);
@@ -91,7 +106,7 @@ class ServerListHandler {
 
 
 		divRoom.addEventListener("click", ()=>{
-			console.log(divRoom.id);
+			ServerListHandler.whenRoomListObjectClicked(divRoom);
 		});
 
 		getElement("#matchmaking-scrollingList").appendChild(divRoom);
@@ -100,6 +115,50 @@ class ServerListHandler {
 		ServerListHandler.refreshRoomList();
 	
 		return 0;
+	}
+
+	static whenRoomListObjectClicked(html_element) {
+		const roomId = html_element.id.replace("room:","");
+		console.log(`${html_element} ; ${roomId}`);
+		if (html_element.mafia_locked) {
+			console.log("It's locked!");
+			getElement("[name='listpart']").style.display = "none";
+			getElement("[name='passwordarea']").style.display = "block";
+			getElement("[name='passwordarea']").mafia_roomId = roomId;
+		} else {
+			console.log("It's not locked");
+			ServerListHandler.sendRoomAccessInformation(roomId, null);
+		}
+	}
+	static sendRoomAccessInformation(roomId, pass) {
+		socket.emit("client->server:request-join-room-with-id", {
+			roomId: roomId,
+			pass: pass
+		}, function(data){
+			if (data.success) {
+				console.log(`Acknowledged - whenRoomListObjectClicked - ${data}`);
+				return true;
+			} else {
+				return false;
+			}
+		});
+		console.log(`User clicked, accessing Room ${roomId}`);
+	}
+
+	static whenRoomPasswordInputSubmit(e) {
+		const evt = e || event;
+		evt.preventDefault();
+		console.log("Tried to input password!");
+		const passwordareaInputNode = getElement("#passwordarea-input");
+		const pass = passwordareaInputNode.value;
+		const roomId = getElement("[name='passwordarea']").mafia_roomId;
+		if (ServerListHandler.sendRoomAccessInformation(roomId, pass)) {
+			console.log("Correct!");
+		} else {
+			console.log("Incorrect");
+			getElement("#passwordarea-error").style.display = "block";
+			getElement("#passwordarea-error").textContent = "Error: 1 attempt remaining.";
+		}
 	}
 
 	static whenRoomCreatorButtonClicked() {
@@ -126,6 +185,7 @@ class ServerListHandler {
 		evt.preventDefault();
 		const errorTextNode = getElement("#room-creator_error");
 		const roomNameNode = getElement("#room-creator_name");
+		const roomLockedNode = getElement("#room-creator_locked");
 		let roomName = roomNameNode.value.trim();
         const whitespaceRegex = /\s+/gi;
 		roomName = roomName.replace(whitespaceRegex, " ");
@@ -142,16 +202,44 @@ class ServerListHandler {
 		errorTextNode.style.display = "none";
 		errorTextNode.textContent = "";
 
+		let roomId = null;
+		const roomPassNode = getElement("#room-creator_password");
+		let passwd = roomPassNode.value.replace(whitespaceRegex, " ");
+		if (passwd === "") passwd = null;
+
 		// console.log("Registered: " + roomName);
 
 		socket.emit("client->server:request-room-creation-with-name", {
-			name: roomName
+			name: roomName,
+			pass: passwd
 		}, function(other){
 			console.log(`Server assigned Room of name ${roomName} with ID: ${other}`);
-			
+			roomId = String(other);
+			const isLocked = roomLockedNode.checked;
+			ServerListHandler.deployRoomListObject(roomId, roomName, isLocked);
+			getElement("#create-room-btn").click();
+			getElement("#create-room-btn").blur();
 		});
 	}
 }
 getElement("#create-room-btn").addEventListener("click", ServerListHandler.whenRoomCreatorButtonClicked);
 getElement("#room-creator_locked").addEventListener("change", ServerListHandler.whenRoomCreatorLockedSliderChanged);
 getElement("#room-creator_submit").addEventListener("click", ServerListHandler.whenRoomCreatorSubmitButtonClicked);
+getElement("#passwordarea-submit").addEventListener("click", ServerListHandler.whenRoomPasswordInputSubmit);
+getElement("#passwordarea-input").addEventListener("keydown", (e)=>{
+	const evt = e || event;
+	if (evt.key === "Enter") getElement("#passwordarea-submit").click();
+});
+
+
+
+class Dev {
+	static logRooms() {
+		socket.emit("log-rooms", {
+			rooms: "all"
+		}, function(other) {
+			console.log("Logged rooms: ");
+			console.log([... other.room]);
+		});
+	}
+}
--- server.js
+++ server.js
@@ -15,7 +15,7 @@ class Client {
 		this.inRoom = false;
 		this.room = null;
 		this.roomId = null;
-		console.log(`[Create] Client ${this.socketId}`);
+		console.log(`[Create] Client ${this.socketId}  :SocketId=${this.socketId}  :InRoom=${this.inRoom}  :RoomId=${this.roomId}`);
 	}
 	_destroy() {
 		if (this.room) {
@@ -28,15 +28,45 @@ class Client {
 		this.room = rooms.get(roomId);
 		console.log(`Client ${this.socketId} set own room to Room ${roomId}`);
 	}
-	joinRoom(roomId) {
-		rooms.get(roomId).addClient(this.socketId);
+	joinRoom(_room, pass) {
+		if (typeof _room === "string") _room = rooms.get(_room);
+		if (_room.addClient(this.socketId, pass)) {
+			this.inRoom = true;
+			this.room = _room;
+			this.roomId = _room.id;
+			return true;
+		} else {
+			this.inRoom = false;
+			this.room = null;
+			this.roomId = null;
+			return false;
+		}
+	}
+	leaveRoom() {
+		if (this.roomId !== null || this.room !== null) {
+			this.room = null;
+			this.roomId = null;
+			this.inRoom = false;
+			return true;
+		}
+		return false;
+	}
+
+	details() {
+		console.log(`Client ${this.socketId} = {
+	socketId: ${this.socketId},
+	socket: [Object Socket - contains circular references],
+	inRoom: ${this.inRoom},
+	room: ${this.room},
+	roomId: ${this.roomId}\n}`);
 	}
 }
 
 function clientAdded(socket) {
 	clients.set(socket.id, new Client(socket.id, socket));
+	socket.mafia_client = clients.get(socket.id);
 	console.log(`[+] ${socket.id}`);
-	console.log(`${socket.id} is in Room ${clients.get(socket.id).roomId}`);
+	socket.mafia_client.details();
 }
 function clientRemoving(socketId) {
 	clients.delete(socketId);
@@ -45,33 +75,65 @@ function clientRemoving(socketId) {
 
 
 class Room {
-	constructor(id, name) {
+	constructor(id, name, pass) {
 		this.id = id;
 		this.name = name || "Room";
+		this.password = pass;
+		this.passwordProtected = (pass !== null) ? true : false;
 		this.clients = new Map();
+		this.numberOfClients = 0;
+		this.maximumNumberOfClients = Infinity;
 
-		console.log(`[Create] Room ${this.id}`);
+		console.log(`[Create] Room ${this.id}  :Name=${this.name}  :Password=${this.password}  :Clients=${[... this.clients.values()]}  :NumberOfClients=${this.numberOfClients}`);
 	}
-	addClient(client) {
+	addClient(client, pass=null) {
+		if (this.numberOfClients >= this.maximumNumberOfClients || pass !== this.password) return false;
 		if (typeof client === "string") client = clients.get(client);
 		this.clients.set(client.id, client);
 		client._setRoomId(this.id);
+		this.numberOfClients = this.clients.size;
 		console.log(`Room ${this.id} added Client ${client.id}`);
+		return true;
 	}
-	removeClient(id) {
-		this.clients.delete(id);
+	removeClient(client) {
+		if (typeof client === "string") client = this.clients.get(client);
+		client.leaveRoom();
+		this.clients.delete(client.id);
+		this.numberOfClients = this.clients.size;
 		console.log(`Room ${this.id} removed Client ${id}`);
+	}
+	clearClients() {
+		for (const client of this.clients.values()) {
+			this.removeClient(client);
+		}
 	}
 
 	details() {
-		console.log(`Room ${this.id}, ${this.name},\n-Clients: ${this.clients}`);
+		console.log(`Room ${this.id} = {
+	id: ${this.id},
+	name: ${this.name},
+	password: ${this.password},
+	clients: ${[... this.clients.values()]},
+	numberOfClients: ${this.numberOfClients}\n}`);
 	}
 
 
-	static createRoom(name="Room") {
+	static createRoom(name, pass=null) {
 		const id = Generics.generateRandomId();
-		rooms.set(id, new Room(id, name));
+		if (name === undefined) name = "Room";
+		rooms.set(id, new Room(id, name, pass));
 		return rooms.get(id);
+	}
+	static destroyRoom(room) {
+		if (typeof room === "string") room = rooms.get(room);
+		room.clearClients();
+		room.numberOfClients = 0;
+		room.maximumNumberOfClients = 0;
+		room.name = "Room about to be destroyed";
+		const roomName = room.name;
+		const roomId = room.id;
+		rooms.delete(roomId);
+		console.log(`[Destroy] Room ${roomId} ${roomName} was destroyed.`);
 	}
 }
 
@@ -92,6 +154,16 @@ class Generics {
 }
 
 
+class RoomHandler {
+	static createRoom(name, pass) {
+		return Room.createRoom(name, pass);
+	}
+	static destroyRoom(room) {
+		return Room.destroyRoom(room);
+	}
+}
+
+
 
 io.on("connection", function(socket) {
 	const socketId = socket.id;
@@ -99,12 +171,39 @@ io.on("connection", function(socket) {
 
 
 	socket.on("client->server:request-room-creation-with-name", (data, ack)=>{
-		const {name} = data;
-		const room = Room.createRoom(name);
+		const {name, pass} = data;
+		const room = RoomHandler.createRoom(name, pass);
 		ack(room.id);
 		console.log(`Created new Room #${room.id} with name ${room.name} - then sent ack with room-id`);
 	});
+
+	socket.on("client->server:request-join-room-with-id", (data, ack)=>{
+		const {roomId, pass} = data;
+		if (socket.mafia_client.joinRoom(String(roomId), pass)) {
+			ack({
+				success: true,
+				room: socket.mafia_client.room
+			});
+			console.log(`Client ${socketId} joined Room ${roomId} successfully`);
+		} else {
+			ack({
+				success: false,
+				error: "Incorrect password"
+			});
+		}
+	});
+
+	socket.on("client->server:request-room-list", (data, ack)=>{
+		ack({
+			rooms: [... rooms]
+		});
+		console.log(`Sent rooms<Map> to Client ${socketId}`);
+	});
 	
+	socket.on("log-rooms", (data, ack)=> {
+		console.log([... rooms.values()]);
+		ack({room: [... rooms.values()]});
+	});
 
 	clientAdded(socket);
 });
